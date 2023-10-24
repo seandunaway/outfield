54d056d41cdc5241fba47408914ef1e74f2c92c5
seandunaway <git@seandunaway.com>
2023-10-24 10:03:35 -0700

    notional

--- index.mjs
+++ index.mjs
@@ -33,32 +33,36 @@ for (let table of tables) {
         let itm = row.classList.contains('in-the-money')
         let strike_text = row.querySelector('td.data-col2 a')?.textContent
         let price_text = row.querySelector('td.data-col3')?.textContent
+        let bid_text = row.querySelector('td.data-col4')?.textContent
+        let ask_text = row.querySelector('td.data-col5')?.textContent
 
-        if (!strike_text || !price_text) throw new Error()
+        if (!strike_text || !price_text || !bid_text || !ask_text) throw new Error()
 
         let strike = parseInt(strike_text)
         let price = parseFloat(price_text)
+        let bid = parseFloat(bid_text)
+        let ask = parseFloat(ask_text)
 
-        options.push({type, itm, strike, price})
+        options.push({type, itm, strike, price, bid, ask})
     }
 }
 
-let call_strike_count = 0
+let call_strike_amount = 0.0
 for (let i = 0; i <= options.length - 1; i++) {
     if (options[i].type !== 'call') continue
     if (options[i].itm) continue
-    call_strike_count++
+    call_strike_amount += options[i].ask
     if (options[i].price <= price_float) break
 }
 
-let put_strike_count = 0
+let put_strike_amount = 0.0
 for (let i = options.length - 1; i >= 0; i--) {
     if (options[i].type !== 'put') continue
     if (options[i].itm) continue
-    put_strike_count++
+    put_strike_amount += options[i].ask
     if (options[i].price <= price_float) break
 }
 
-if (!call_strike_count && !put_strike_count) process.exit(1)
+if (!call_strike_amount && !put_strike_amount) process.exit(1)
 
-console.info(`+${call_strike_count} -${put_strike_count}`)
+console.info(`+${call_strike_amount.toFixed(2)} -${put_strike_amount.toFixed(2)}`)
